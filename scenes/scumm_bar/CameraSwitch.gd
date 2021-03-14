6582b33dd6a24cae73388b3cd100a9738d637637
KFischer (MBP) <klaus.fischer@eloware.com>
2021-03-14 10:57:42 +0100

    Moving camera

---
+++ scenes/scumm_bar/CameraSwitch.gd
@@ -0,0 +1,28 @@
+extends CollisionPolygon2D
+
+
+export(NodePath) var camera
+var cam: Camera2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	cam = get_node(camera)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CameraSwitch_body_entered(body):
+	if cam.position.x > 320:
+		cam.position = Vector2(160,100)
+	else:
+		cam.position = Vector2(480,100)
+	pass # Replace with function body.
